512d114ce875d335359f47bb2ee5bd35786ef99d
subhamay2002 <subhamaynath2002@gmail.com>
2022-03-22 19:10:18 +0530

    carousel

--- Component/Navbar.js
+++ Component/Navbar.js
@@ -10,43 +10,43 @@ export default function Navbar() {
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </Head>
             <div className="container-fluid bg-pink">
-                <div className="container bg-pink">
-                    <nav className="navbar navbar-expand-lg navbar-light bg-pink">
-                        <a className="navbar-brand pal-1" href="index.html">
-                            <img src="images/logo.png" width="80" height="80" alt="logo.png" />
-                        </a>
-                        <button
-                            className="navbar-toggler"
-                            type="button"
-                            data-toggle="collapse"
-                            data-target="#navbarNav"
-                            aria-controls="navbarNav"
-                            aria-expanded="false"
-                            aria-label="Toggle navigation"
-                        >
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarNav">
-                            <ul className="navbar-nav pal-6">
-                                <li className="nav-item pl-3">
-                                    <a className="nav-link" href="#">Store</a>
-                                </li>
-                                <li className="nav-item pl-3">
-                                    <a className="nav-link" href="#">Consult</a>
-                                </li>
-                                <li className="nav-item pl-3">
-                                    <a className="nav-link" href="#">About Us</a>
-                                </li>
-                                <li className="nav-item pl-3">
-                                    <a className="nav-link fs-1" href="#">
-                                        <i className="fa fa-user-circle" aria-hidden="true"></i>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
-                </div>
-            </div>
+      <div className="container bg-pink">
+        <nav className="navbar navbar-expand-lg navbar-light bg-pink">
+          <Link href="/">
+            <img src="images/logo.png" width="80" height="80" alt="logo.png" />
+            </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav pal-6">
+              <li className="nav-item pl-3">
+                <a className="nav-link" href="#">Store</a>
+              </li>
+              <li className="nav-item pl-3">
+                <a className="nav-link" href="#">Consult</a>
+              </li>
+              <li className="nav-item pl-3">
+                <a className="nav-link" href="#">About Us</a>
+              </li>
+              <li className="nav-item pl-3">
+                <a className="nav-link fs-1" href="#">
+                  <i className="fa fa-user-circle" aria-hidden="true"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
         </>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,122 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  
-  return (
-    <div className={styles.container}>
-    
 
-      
+  return (
+    <div >
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-xl-12">
+            <div
+              id="carouselExampleIndicators"
+              className="carousel slide"
+              data-ride="carousel"
+            >
+              <ol className="carousel-indicators">
+                <li
+                  data-target="#carouselExampleIndicators"
+                  data-slide-to="0"
+                  className="active"
+                ></li>
+                <li
+                  data-target="#carouselExampleIndicators"
+                  data-slide-to="1"
+                ></li>
+                <li
+                  data-target="#carouselExampleIndicators"
+                  data-slide-to="2"
+                ></li>
+              </ol>
+              <div className="carousel-inner">
+                <div className="carousel-item active">
+                  <img
+                    className="d-block w-100"
+                    src="images/banner 1.jpg"
+                    alt="First slide"
+                  />
+                  <div className="carousel-caption d-none d-md-block">
+                    <h1 className="text-1 wow fadeInLeftBig">Join our revolution</h1>
+                    <p className="text-2 wow fadeInRightBig">Donate for stray dogs</p>
+                    <button type="button" className="btn btn-outline-lights">
+                      Donate<i
+                        className="fa fa-long-arrow-right ml-1"
+                        aria-hidden="true"
+                      ></i>
+                    </button>
+                  </div>
+                </div>
+                <div className="carousel-item">
+                  <img
+                    className="d-block w-100"
+                    src="images/banner 2.jpg"
+                    alt="Second slide"
+                  />
+                  <div className="carousel-caption d-none d-md-block">
+                    <h1 className="text-1 wow fadeInLeftBig">
+                      World className vets for your dog
+                    </h1>
+                    <p className="text-2 wow fadeInRightBig">
+                      Consult with our vets for your dogs health and a beautiful
+                      life
+                    </p>
+                    <button type="button" className="btn btn-outline-lights">
+                      Talk Now<i
+                        className="fa fa-long-arrow-right ml-1"
+                        aria-hidden="true"
+                      ></i>
+                    </button>
+                  </div>
+                </div>
+                <div className="carousel-item">
+                  <img
+                    className="d-block w-100"
+                    src="images/banner 3.jpg"
+                    alt="Third slide"
+                  />
+                  <div className="carousel-caption d-none d-md-block">
+                    <h1 className="text-1 wow fadeInLeftBig">Buy best</h1>
+                    <p className="text-2 wow fadeInRightBig">
+                      Buy medecines and other essentials for your dog
+                    </p>
+                    <button type="button" className="btn btn-outline-lights">
+                      Buy Now<i
+                        className="fa fa-long-arrow-right ml-1"
+                        aria-hidden="true"
+                      ></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <a
+                className="carousel-control-prev"
+                href="#carouselExampleIndicators"
+                role="button"
+                data-slide="prev"
+              >
+                <span
+                  className="carousel-control-prev-icon"
+                  aria-hidden="true"
+                ></span>
+                <span className="sr-only">Previous</span>
+              </a>
+              <a
+                className="carousel-control-next"
+                href="#carouselExampleIndicators"
+                role="button"
+                data-slide="next"
+              >
+                <span
+                  className="carousel-control-next-icon"
+                  aria-hidden="true"
+                ></span>
+                <span className="sr-only">Next</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
     </div>
   )
 }
