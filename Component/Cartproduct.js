51677f38a248d954a46bb4e9f827b7389536614b
subhamay2002 <subhamaynath2002@gmail.com>
2022-05-28 10:43:37 +0530

    cart delete working

--- Component/Cartproduct.js
+++ Component/Cartproduct.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import baseUrl from '../helpers/baseUrl';
 export const Cartproduct = (props) => {
-    var {product } =props;
-    
+    var { product } = props;
+
     const id = product.product;
     const [data, setData] = useState();
     useEffect(async () => {
@@ -11,36 +11,49 @@ export const Cartproduct = (props) => {
         const data = await res.json();
         setData(data);
     }, [id]);
-    const updatecartproquatity = async (id,quantity) => {
+    const updatecartproquatity = async (id, quantity) => {
         let newArr = props.cart.map((item, i) => {
-            if (item._id===id) {
+            if (item._id === id) {
                 item.quantity = quantity;
             }
             return item;
-          });
-          props.setCart(newArr);
-          
+        });
+        props.setCart(newArr);
+
 
     }
-  return (
-    <tr>
-                        <td>
-                            <div className="cart-info">
-                                {data &&
-                                    <img src={data.image} />}
-                                <div className="mt-5 mt-md-4 mt-xs-5 ml-xs-5">
-                                    <p className="text-cart-2">{product.productname}</p>
-                                    <small className="text-cart-3">Price: ${product.price}.00</small>
-                                    <br />
-                                    <a href="#">Remove</a>
-                                </div>
-                            </div>
-                        </td>
-                        <td>{data&&
-                            <input type="number" min="1" value={product.quantity} max={data.quantity}
-                            onChange={(e)=>{updatecartproquatity(product._id,e.target.value)}}
-                        />}</td>
-                        <td>${product.quantity*product.price}.00</td>
-                    </tr>
-  )
+    const removeproduct = async () => {
+        const res = await fetch(`${baseUrl}/api/Cart`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                productId: product.product
+            })
+        });
+        const data = await res.json();
+        props.setCart(data.products);
+    }
+    return (
+        <tr>
+            <td>
+                <div className="cart-info">
+                    {data &&
+                        <img src={data.image} />}
+                    <div className="mt-5 mt-md-4 mt-xs-5 ml-xs-5">
+                        <p className="text-cart-2">{product.productname}</p>
+                        <small className="text-cart-3">Price: ${product.price}.00</small>
+                        <br />
+                        <a href='#' onClick={removeproduct}>Remove</a>
+                    </div>
+                </div>
+            </td>
+            <td>{data &&
+                <input type="number" min="1" value={product.quantity} max={data.quantity}
+                    onChange={(e) => { updatecartproquatity(product._id, e.target.value) }}
+                />}</td>
+            <td>${product.quantity * product.price}.00</td>
+        </tr>
+    )
 }
--- pages/api/Cart.js
+++ pages/api/Cart.js
@@ -2,50 +2,18 @@ import Cart from "../../Models/Cart";
 import initDB from "../../helpers/initDB";
 initDB();
 export default async (req, res) => {
-    const { user } = req.cookies;
+    
     switch (req.method) {
         case "GET":
             await fetchusercart(req, res);
             break;
         case "POST":
-            const { product} = req.body;
-            console.log(product);
-            const cart = await Cart.findOne({ userId: user });
-            if (cart) {
-                cart.products.push({
-                    product: product.productId,
-                    quantity: 1,
-                    price: product.price,
-                    productname: product.productname
-                });
-                console.log(cart)
-                await cart.save();
-
-                res.status(200).json(cart);
-            }
-            else {
-                res.status(404).json({ message: "Cart not found" });
-            }
+            await postcart(req, res);
             break;
+            
         case "DELETE":
-            // const { product } = req.body;
-            // const cart1 = await Cart.findOne({ userId: user });
-            // if (cart1) {
-            //     const item = cart1.products.find(
-            //         (findproduct) => findproduct._id === product._id
-            //     );
-            //     console.log(product);
-            //     console.log(item)
-            //     if (item) {
-            //         cart1.products.splice(cart1.products.indexOf(item), 1);
-            //         await cart1.save();
-            //         res.status(200).json({ message: "Product removed from cart" });
-            //     }
-            //     res.status(200).json({ message: "Product not found in cart" });
-            // }
-            // else {
-            //     res.status(404).json({ message: "Cart not found" });
-            // }
+            await deletecart(req, res);
+            break;
 
     }
 }
@@ -63,5 +31,40 @@ const fetchusercart = async (req, res) => {
     } catch {
         res.status(500).json({ message: "Something went wrong" });
     }
+
+}
+const postcart = async (req, res) => {
+    const { user } = req.cookies;
+    const { product} = req.body;
+    //console.log(product);
+    const cart = await Cart.findOne({ userId: user });
+    if (cart) {
+        cart.products.push({
+            product: product.productId,
+            quantity: 1,
+            price: product.price,
+            productname: product.productname
+        });
+        //console.log(cart)
+        await cart.save();
+
+        res.status(200).json(cart);
+    }
+    else {
+        res.status(404).json({ message: "Cart not found" });
+    }
+}
+const deletecart = async (req, res) => {
+    const { user } = req.cookies;
+    const { productId } = req.body;
+    const cart = await Cart.findOne({ userId: user });
+    
+    cart.products.forEach((item, i) => {
+        if (item.product == productId) {
+            cart.products.splice(i, 1);
+            cart.save();
+        }
+    })
+    res.status(200).json(cart);
 
 }
