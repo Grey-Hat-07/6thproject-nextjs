5d239b9e4760211880070dd4e5d83f34cefc682d
FallenCap <fallencap18@gmail.com>
2022-06-21 19:46:04 +0530

    Update Navbar.js

--- Component/Navbar.js
+++ Component/Navbar.js
@@ -1,9 +1,9 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
-import jsCookie from 'js-cookie';
-import { parseCookies } from 'nookies';
+import Head from "next/head";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import jsCookie from "js-cookie";
+import { parseCookies } from "nookies";
 export default function Navbar() {
   const router = useRouter();
   const { vet } = parseCookies();
@@ -11,16 +11,27 @@ export default function Navbar() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [product, setProduct] = useState(true);
   useEffect(() => {
-    if (router.pathname === '/Login' || router.pathname === '/Signup/Customersignup' || router.pathname === '/Signup/Vetsignup' || router.pathname === '/Signup/adminsign') {
-      setIsLoggedIn(false)
+    if (
+      router.pathname === "/Login" ||
+      router.pathname === "/Signup/Customersignup" ||
+      router.pathname === "/Signup/Vetsignup" ||
+      router.pathname === "/Signup/adminsign"
+    ) {
+      setIsLoggedIn(false);
     } else {
-      setIsLoggedIn(true)
+      setIsLoggedIn(true);
     }
-    if (router.pathname === '/Dashboard' || router.pathname === '/Vetchat' || router.pathname === '/vetaccount' || router.pathname === '/vet-dashboard' || router.pathname === '/productup' || router.pathname === '/accountdashboard') {
-      setIsdashboard(true)
-    }
-    else {
-      setIsdashboard(false)
+    if (
+      router.pathname === "/Dashboard" ||
+      router.pathname === "/Vetchat" ||
+      router.pathname === "/vetaccount" ||
+      router.pathname === "/vet-dashboard" ||
+      router.pathname === "/productup" ||
+      router.pathname === "/accountdashboard"
+    ) {
+      setIsdashboard(true);
+    } else {
+      setIsdashboard(false);
     }
     // if (router.pathname === '/product/id') {
     //   setProduct(true)
@@ -28,7 +39,7 @@ export default function Navbar() {
     // else {
     //   setProduct(false)
     // }
-  }, [router.pathname])
+  }, [router.pathname]);
 
   return (
     <>
@@ -38,14 +49,18 @@ export default function Navbar() {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      {!isdashboard &&
+      {!isdashboard && (
         <div className="container-fluid bg-pink">
           <div className="container bg-pink">
             <nav className="navbar navbar-expand-lg navbar-light bg-pink">
-              <a href="/" className="navbar-brand pal-1" >
-                <img src="/images/logo.png"
-                  width="80" height="80"
-                  className="w-sm" alt="logo.png" />
+              <a href="/" className="navbar-brand pal-1">
+                <img
+                  src="/images/logo.png"
+                  width="80"
+                  height="80"
+                  className="w-sm"
+                  alt="logo.png"
+                />
               </a>
               <button
                 className="navbar-toggler"
@@ -58,7 +73,8 @@ export default function Navbar() {
               >
                 <span className="navbar-toggler-icon"></span>
               </button>
-              {!isLoggedIn && <div className="collapse navbar-collapse" id="navbarNav">
+              {!isLoggedIn && (
+                <div className="collapse navbar-collapse" id="navbarNav">
                   <ul className="navbar-nav pal-6">
                     <li className="nav-item dropdown pl-3">
                       <a
@@ -72,31 +88,62 @@ export default function Navbar() {
                       >
                         <i className="fa fa-user-circle" aria-hidden="true"></i>
                       </a>
-                      <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <div
+                        className="dropdown-menu"
+                        aria-labelledby="navbarDropdown"
+                      >
                         {/* <a className="dropdown-item" href="login I.html">Customer &<br />Paitient Login</a> */}
-                        <a className=' m-2' onClick={
-                          () => {
-                            router.push('/Signup/adminsign')
-                          }
-                        }>Signup as Admin</a></div>
+                        <li>
+                          <a
+                            className=" m-2 texts-2"
+                            onClick={() => {
+                              router.push("/Signup/adminsign");
+                            }}
+                          >
+                            Signup as Admin
+                          </a>
+                        </li>
+
+                        <li>
+                          <a
+                            className=" m-2 texts-2"
+                            onClick={() => {
+                              router.push("/Signup/Vetsignup");
+                            }}
+                          >
+                            Signup as Vet
+                          </a>
+                        </li>
+                      </div>
                     </li>
                   </ul>
                 </div>
-              }
-              {isLoggedIn ?
+              )}
+              {isLoggedIn ? (
                 <div className="collapse navbar-collapse" id="navbarNav">
                   <ul className="navbar-nav pal-6">
                     <li className="nav-item pl-3">
-                      <a className="nav-link" href="/Store">Store</a>
+                      <a className="nav-link" href="/Store">
+                        Store
+                      </a>
                     </li>
                     <li className="nav-item pl-3">
-                      <a className="nav-link" href="/chat">Consult</a>
+                      <a className="nav-link" href="/chat">
+                        Consult
+                      </a>
                     </li>
                     <li className="nav-item pl-3">
-                      <a className="nav-link" href="#">About Us</a>
+                      <a className="nav-link" href="#">
+                        About Us
+                      </a>
                     </li>
                     <li className="nav-item pl-3">
-                      <a href="/cart" className='nav-link'><i className="fa fa-shopping-cart" aria-hidden="true"></i></a>
+                      <a href="/cart" className="nav-link">
+                        <i
+                          className="fa fa-shopping-cart"
+                          aria-hidden="true"
+                        ></i>
+                      </a>
                     </li>
                     <li className="nav-item dropdown pl-3">
                       <a
@@ -110,21 +157,32 @@ export default function Navbar() {
                       >
                         <i className="fa fa-user-circle" aria-hidden="true"></i>
                       </a>
-                      <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <div
+                        className="dropdown-menu"
+                        aria-labelledby="navbarDropdown"
+                      >
                         {/* <a className="dropdown-item" href="login I.html">Customer &<br />Paitient Login</a> */}
-                        <button className='btn btn-danger m-2' onClick={
-                          () => {
-                            jsCookie.remove('user'); if (vet) { jsCookie.remove('vet') }
-                            router.push('/Login');
-                          }
-                        }>logout</button></div>
+                        <button
+                          className="btn btn-danger m-2"
+                          onClick={() => {
+                            jsCookie.remove("user");
+                            if (vet) {
+                              jsCookie.remove("vet");
+                            }
+                            router.push("/Login");
+                          }}
+                        >
+                          logout
+                        </button>
+                      </div>
                     </li>
                   </ul>
-                </div> : null}
+                </div>
+              ) : null}
             </nav>
           </div>
-        </div>}
-
+        </div>
+      )}
     </>
-  )
+  );
 }
