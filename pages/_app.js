e985b7f58a8761918f235792edbc8b5804edd7f5
subhamay2002 <subhamaynath2002@gmail.com>
2022-06-22 07:00:43 +0530

    commits

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import Layout from '../Component/Layout'
 // import { StrictMode } from 'react'
+//
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
