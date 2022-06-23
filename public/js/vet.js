f1be68b8716f6f0e26d82b7277518752eeee7730
FallenCap <fallencap18@gmail.com>
2022-06-23 21:38:45 +0530

    Create vet.js

---
+++ public/js/vet.js
@@ -0,0 +1,6 @@
+const logo = document.querySelector(".logo-vet");
+const clicking = document.querySelector("#clicking");
+
+clicking.addEventListener("click", function () {
+  logo.classList.toggle("logo-vet-1");
+});
