08c428e6c0cc334657d73e105653c396fc7ab139
Subhamay Nath <63004195+Grey-Hat-07@users.noreply.github.com>
2023-07-20 23:28:18 +0530

    Update baseUrl.js

--- helpers/baseUrl.js
+++ helpers/baseUrl.js
@@ -1,3 +1,3 @@
-const baseUrl = process.env.NODE_ENV === 'production' ? 'https://pawlife-git-master-grey-hat-07.vercel.app' : 'http://localhost:3000';
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://pawlife.vercel.app' : 'http://localhost:3000';
 
 export default baseUrl;
